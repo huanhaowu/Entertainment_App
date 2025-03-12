46645fba21449b811f44d11c682e5d69246b1556
huanhaowu <1104326@est.intec.edu.do>
2025-03-11 22:35:20 -0400

    feat: modify navbar

--- src/app/components/navigation/Navbar.tsx
+++ src/app/components/navigation/Navbar.tsx
@@ -1,16 +1,45 @@
 import Link from "next/link";
+import { Clapperboard, LayoutGrid, Film, Tv, Bookmark} from 'lucide-react';
 
 export default function Navbar() {
     return (
         <nav className="bg-gray-900 text-white p-4">
-            <div className="container mx-auto flex justify-between items-center">
-                <h1 className="text-2xl font-bold">Entertainment App</h1>
-                <ul className="flex space-x-6">
-                    <li><Link href="/">Home</Link></li>
-                    <li><Link href="/discover">Discover</Link></li>
-                    <li><Link href="/movies">Movies</Link></li>
-                    <li><Link href="/series">Series</Link></li>
-                    <li><Link href="/bookmark">Bookmarked</Link></li>
+            <div className="container mx-auto flex flex-col justify-between items-center">
+                <ul className="flex flex-col w-fit gap-y-4">
+                    <li className="flex items-center space-x-2">
+                        <Link href="/" className="flex items-center space-x-2">
+                            <Clapperboard className="w-5 h-5"/>
+                            <span>Home</span>
+                        </Link>
+                    </li>
+
+                    <li className="flex items-center space-x-2">
+                        <Link href="/discover" className="flex items-center space-x-2">
+                            <LayoutGrid  className="w-5 h-5"/>
+                            <span>Discover</span>
+                        </Link>
+                    </li>
+
+                    <li className="flex items-center space-x-2">
+                        <Link href="/movies" className="flex items-center space-x-2">
+                            <Film className="w-5 h-5"/>
+                            <span>Movies</span>
+                        </Link>
+                    </li>
+
+                    <li className="flex items-center space-x-2">
+                        <Link href="/series" className="flex items-center space-x-2">
+                            <Tv className="w-5 h-5"/>
+                            <span>Series</span>
+                        </Link>
+                    </li>
+
+                    <li className="flex items-center space-x-2">
+                        <Link href="/bookmark" className="flex items-center space-x-2">
+                            <Bookmark className="w-5 h-5"/>
+                            <span>Bookmarked</span>
+                        </Link>
+                    </li>
                 </ul>
             </div>
         </nav>
